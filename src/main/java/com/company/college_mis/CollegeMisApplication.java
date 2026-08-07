62174ba8b1f6f917364155d342f885142d4b33d4
Chandrakant <chandrakantyetre@gmail.com>
2026-08-07 18:47:28 +0530

    Saved in new repository

---
+++ src/main/java/com/company/college_mis/CollegeMisApplication.java
@@ -0,0 +1,13 @@
+package com.company.college_mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/college_mis/CollegeMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.college_mis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
